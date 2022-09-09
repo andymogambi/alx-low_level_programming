49aa06497bc986cc0fc2a79242c9a7a7008370d1
Andrew Anyona <anyonaandrew7@gmail.com>
2022-09-09 07:42:04 -0700

    c 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+/**
+ * main - Printing
+ *
+ * Return: Always 0 (Success) 
+ */
+int main(void) 
+{
+	puts("\ "Programming is like building a multilingual puzzle ") ;
+	return (0) ;
