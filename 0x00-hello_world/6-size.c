a55da39e6492111992c2aea50d4c065306d9b320
Andrew Anyona <anyonaandrew7@gmail.com>
2022-09-09 11:05:35 -0700

    c 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print the string in the function
+ * main - Prints the size of many var types
  * Description: using the main function
  * this program prints the results of the function
  * Return: 0
@@ -13,10 +13,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %id byte(s)\n", sizeof (c));
-	printf("Size of an int: %id byte(s)\n", sizeof (i));
-	printf("Size of long int: %id byte(s)\n", sizeof (li));
-	printf("Size of a long long int: %id byte(s)\n", sizeof (lli));
+	printf("Size of a char: %id byte(s)\n", sizeof(c));
+	printf("Size of an int: %id byte(s)\n", sizeof(i));
+	printf("Size of long int: %id byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %id byte(s)\n", sizeof(lli));
 	printf("Size of float: %id byte(s)\n", sizeof(f));
 	return (0);
 }
