f441d0352998d1b7610d33496ba4b5d35a31c054
Andrew Anyona <anyonaandrew7@gmail.com>
2022-09-09 08:21:32 -0700

    c 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h> 
-
-
 /**
  * main - print the string in the put function
  * Description: using the main function
@@ -12,4 +10,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-:%s/\ //g
