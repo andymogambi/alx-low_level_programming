25e15e078286ac2b858cf9675069e77d3c383df5
Andrew Anyona <anyonaandrew7@gmail.com>
2022-09-09 08:13:57 -0700

    c 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,6 @@
 
 /**
  * main - print the string in the put function
- *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
