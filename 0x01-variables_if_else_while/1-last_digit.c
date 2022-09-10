a843a52207cf677fa494eeea1b2edaf4d600aa90
Andrew Anyona <anyonaandrew7@gmail.com>
2022-09-10 13:59:14 -0700

    var 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,41 +1,37 @@
-#include <stdlib.h>                                                                                                                                    
+#include <stdlib.h>
 
-#include <time.h>                                                                                                                                      
+#include <time.h>
 
-#include <stdio.h>                                                                                                                                     
-
-/**                                                                                                                                                    
- *
- * *main - print if the number is positive, negative or zero                                                                                              
- *
- * *Description: using the main function                                                                                                                  
- *
- * *this program prints "Programming is positive, zero or negative                                                                                        
- *
- * *Return: 0                                                                                                                                             
- *
- * */                                                                                                                                                     
-
+#include <stdio.h>
+/**
+* main - print if the number is postive, zero, or negative
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
 int main(void)
 {
 	int n;
-	int 1;
-	
-	srand(time(0));
-	n - rand() - RAND_MAX / 2;
-	1 - n % 10;
+	int l;
 
-	if (1 > 5) 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+	if (l > 5)
+
 	{
-		printf("Last digit of %id is %id and is greater than 5\n" , n, 1);
+			printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else if (1 == 0)
+
+	else if (l == 0)
+
 	{
-		printf("Last digit of %id is %id and is 0\n" , n, 1);
+			printf("Last digit of %d is %d and is 0\n", n, l);
 	}
+
 	else
 	{
-		printf("Last digit of %id is %id and is less than 6 and not 0\" , n, 1);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
 }
