ee519af452153298adc89003725ebb4075364345
Andrew Anyona <anyonaandrew7@gmail.com>
2022-09-10 15:40:43 -0700

    var 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,24 +8,30 @@
 */
 
 int main(void)
-
+	
 {
 
-	 int c;
+	int c;
 
-	 for (c = 48; c <= 57; c++)
-	                  {
+	for (c = 48; c <= 57; c++)
 
-	 putchar(c);
+	{	
+			putchar(c);
+	
+			if (c != 57)
+	
+			{
+			
+						putchar(',');
+			
+						putchar(' ');
+			
+					}
+	
+		}
 
-	 if (c != 57)
-												{
-	 putchar(',');
-																				putchar(' ');
-																				}
+	putchar('\n');
 
-	 putchar('\n');
-
-					return (0);
+	return (0);
 
 }
