a8bddf07fbc5b969f4ab19a4793e44895c97f4e4
Andrew Anyona <anyonaandrew7@gmail.com>
2022-09-28 02:18:49 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,37 @@
+/**
+ * file 0-puts_recursion.c
+ * Auth Andy
+ */
+
 #include "main.h"                                                                                                                       
-#include <unistd.h>                                                                                                                     
-                                                                                                                                        
+                                                                                                                                       
 /**                                                                                                                                     
- * _putchar - writes the character c to stdout                                                                                          
+ * _puts_recursion - Prints a string, followed by a new line.                                                                           
  *
- * @c: The character to print                                                                                                           
- *
- * Return: On success 1.                                                                                                                
- *
- * On error, -1 is returned, and errno is set appropriately.                                                                            
+ * @s: The string to be printed.                                                                                                        
  *
  */                                                                                                                                     
 
-int _putchar(char c)                                                                                                                    
+void _puts_recursion(char *s)                                                                                                           
 
 {                                                                                                                                       
 
-	        return (write(1, &c, 1));                                                                                                       
+	        if (*s)                                                                                                                         
+
+			        {                                                                                                                               
+
+					                _putchar(*s);                                                                                                           
+
+							                _puts_recursion(s + 1);                                                                                                 
+
+									        }                                                                                                                               
+
+		                                                                                                                                        
+
+		        else                                                                                                                            
+
+				                _putchar('\n');                                                                                                         
 
 }
+
+
